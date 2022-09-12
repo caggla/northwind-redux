3d5506d4f068f0bd113cf0cea49bfe0fdf2221dd
caglacorakk <corakcagla021@gmail.com>
2022-09-13 00:18:33 +0300

    Connect component to Redux with connect

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+//artık bu component i connect ile bağlayabiliriz. 6. adım :D
 
-export default class CategoryList extends Component {
+class CategoryList extends Component {
   render() {
     return (
       <div>
         <h3>Categories</h3>
+        <h5>Selected Category: {this.props.currentCategory.categoryName}</h5>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer, //bu sekılde currentCategory nin props unu almıs olduk kısaca :D
+  };
+}
+
+export default connect(mapStateToProps)(CategoryList);
--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -1,7 +1,64 @@
 import React from "react";
+//import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 export default class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Navbar color="light" light>
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle()} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu end>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
