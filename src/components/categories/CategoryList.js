22e6b1297e065d4414cc4d90bce5053ba67be841
caglacorakk <corakcagla021@gmail.com>
2022-09-12 23:16:02 +0300

    File Structure

---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CategoryList extends Component {
+  render() {
+    return <div>CategoryList</div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/root/App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
