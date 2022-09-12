aa2b0b123e7d56ec426ab5c26f1cf8e37a4f9b62
caglacorakk <corakcagla021@gmail.com>
2022-09-12 23:40:55 +0300

    Category Action created

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,5 @@
+//kategoriye tıklandıgında product a ulaşmıs olcaz yani veriyi ordan oraya atacagız. Yani redux ta bu bizim için bir STATE .:D
+//redux mı normal prop mu nasıl karar veirriz. mesela category componentini biz sadece burada kullanacaksak props yeterli, fakat bu componenti başka yerlerde kullanıp degıstıreceksek falan redux gerekir . :D
+//yani mesela ürün eklicez o halde kategorisini de ekleme sayfasında sececegız urunun.
+export const CHANGE_CATEGORY = "CHANGE_CATEGORY";
+export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS"; // APİDEN DOĞRU RESPONSE GELIP GELMEDIGINE BAKICAZ.
